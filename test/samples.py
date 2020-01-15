407ceb4aa4f9fe3cf345b6cf5a86ae5d489bca6d
Wing Yan Wong <wing_yan_wong@brown.edu>
2020-01-15 17:56:15 -0600

    plotting script added

---
+++ test/samples.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+Lumi = 1
+
+bkgsamples = {
+    "QCD": [   
+        "mttbar_testQCD.root",
+    ],
+    "WJets": [
+        "mttbar_testWJets.root",
+    ]
+}
+
+data = [
+    "mttbar_testEle2017B.root",
+]
+
+signal = [
+    "mttbar_testRSG1000.root",
+]
+
+weight = {}
+weightFile = open('weights.txt','r')
+lines = weightFile.readlines()
+for l in lines:
+    line = (l.split('\n')[0]).split(',')
+    sampleName = line[0]
+    NEff = float(line[1])
+    Xsec = float(line[2])
+    weight[sampleName] = Lumi*Xsec/NEff
