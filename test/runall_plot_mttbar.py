5a08e682065246d43d6c775b0f51f9e08bcb95c8
acwillia <acwillia@buffalo.edu>
2020-01-16 18:08:16 -0600

    runall_plot_mttbar.py template

--- test/runall_plot_mttbar.py
+++ test/runall_plot_mttbar.py
@@ -11,10 +11,18 @@ import sys
 import array as array
 from plot_mttbar import plot_mttbar
 
+TT_0lep_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/TT_0lep.root", "--file_out", "TT_1lep_plots_elec_T1B1.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
+TT_1lep_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/TT_1lep.root", "--file_out", "TT_1lep_plots_elec_T1B1.root", "--leptonType", " electron", "--bTag", "TopTag" ]
+TT_2lep_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/TT_2lep.root", "--file_out", "TT_1lep_plots_elec_T1B1.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
 
-ttjetsStr = ["--file_in", "ttjets.root", "--file_out", "ttjets_plots.root"]
-rsgluon3TeVStr = ["--file_in", "rsgluon_ttbar_3TeV.root", "--file_out", "rsluon3TeV_plots.root"]
-singlemuStr = ["--file_in", "singlemuon.root", "--file_out", "singlemuon_plots.root", "--isData"]
+#----------------------------------------------------------------------------------------------------------------------#
+
+QCD_1000to1500_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/QCD_1000to1500.root", "--file_out", "TT_1lep_plots.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
+QCD_1000to1500_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/QCD_1000to1500.root", "--file_out", "TT_1lep_plots.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
+QCD_1500to2000_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/QCD_1500to2000.root", "--file_out", "TT_1lep_plots.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
+QCD_1500to2000_Str = ["--file_in", "root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/QCD_2000to2000.root", "--file_out", "TT_1lep_plots.root", "--leptonType", " electron", "--bTag", "--TopTag" ]
+
+
 
 plot_mttbar( ttjetsStr )
 plot_mttbar( rsgluon3TeVStr )
