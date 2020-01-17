cc28b82189295861e1e406ec9ee37a5cd7114ec9
Austin Townsend <atownse2@nd.edu>
2020-01-16 19:52:04 -0600

    Update to runall...

--- test/runall_plot_mttbarsimple.py
+++ test/runall_plot_mttbarsimple.py
@@ -14,13 +14,19 @@ import os
 
 tTag = False
 bTag = True
-lepton = 'muon'
+lepton = 'electron'
+
+
 path = 'root://cmseos.fnal.gov//store/user/cmsdas/2020/long_exercises/B2GTTbar/update2017data/'
 ext = '.root'
 
+if lepton == 'electron':lep = 'El'
+else: lep = 'Mu'
 
-dirName = 'plots_Mu_T0B1'
-#os.mkdir(dirName)
+dirName = 'plots_' + lep + '_T' + str(int(tTag)) + 'B' + str(int(bTag))
+
+if not os.path.isdir(dirName):
+    os.mkdir(dirName)
 
 mc = ['RSG_1000','TT_0lep','TT_1lep','TT_2lep','ST_s_tbar','ST_s_tbar','ST_s_top','ST_tW_tbar','ST_tW_top','ST_t_tbar','ST_t_top','WJets_100to250','WJets_250to400','WJets_400to600','WJets_50to100','WJets_600toInf','QCD_1000to1500','QCD_1500to2000','QCD_2000toInf','QCD_200to300','QCD_300to500','QCD_500to700','QCD_700to1000']
 
